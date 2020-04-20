9af9173529928cb1bde2855f116af94995e06437
liortra <lior.trachtman@s.afeka.ac.il>
2020-04-20 18:45:02 +0300

    update HTBio file

--- scripts/Lepton_API.py
+++ scripts/Lepton_API.py
@@ -7,7 +7,6 @@ import numpy
 import time
 from datetime import datetime
 import struct
-import binascii
 
 # init and const
 numpyArr = None
@@ -18,34 +17,42 @@ fout = open(filename, 'wb')
 version_id = 1  # 1 byte
 patient_id = 11  # 4 byte
 test_id = 111  # 4 byte
-# split time to array of int
-timestampString = datetime.now()
-print(timestampString)
-dt_string = timestampString.strftime("%d/%m/%Y %H:%M:%S")
-line = ''.join([i for i in dt_string if i.isdigit()])
-line = [char for char in line]  # 1 byte
-#
+# this section creates the date object
+dateTimeObj = datetime.now()
+now = dateTimeObj.strftime("%d%m%Y%H%M%S")  # ddMMyyyyHHmmss
+dateX = now.encode(encoding='ascii', errors='strict')
 frame_width = 160  # 4 byte
 frame_height = 120  # 4 byte
 number_of_frames = 10  # 4 byte
-decay_point = 2  # 4 byte
-heating_point = 5  # 4 byte
+decay_point = 5  # 4 byte
+heating_point = 2  # 4 byte
 
 
 # https://docs.python.org/3.4/library/struct.html#format-strings
 def buildHeader():
-    fout.write("HEADER".encode('ascii'))
-    fout.write(binascii.hexlify(struct.pack('<b', version_id)))
-    fout.write(binascii.hexlify(struct.pack('<i', patient_id)))
-    fout.write(binascii.hexlify(struct.pack('<i', test_id)))
-    # write the current time
-    [fout.write(binascii.hexlify(struct.pack('<b', int(i)))) for i in line]
-    #
-    fout.write(binascii.hexlify(struct.pack('<i', frame_width)))
-    fout.write(binascii.hexlify(struct.pack('<i', frame_height)))
-    fout.write(binascii.hexlify(struct.pack('<i', number_of_frames)))
-    fout.write(binascii.hexlify(struct.pack('<i', decay_point)))
-    fout.write(binascii.hexlify(struct.pack('<i', heating_point)))
+    # fout.write("HEADER".encode('ascii'))
+
+    fout.write(struct.pack('B', version_id))
+    fout.write(struct.pack('i', patient_id))
+    fout.write(struct.pack('i', test_id))
+    fout.write(struct.pack('14B', *dateX))
+    fout.write(struct.pack('i', number_of_frames))
+    fout.write(struct.pack('i', frame_width))
+    fout.write(struct.pack('i', frame_height))
+    fout.write(struct.pack('i', decay_point))
+    fout.write(struct.pack('i', heating_point))
+
+    # fout.write(binascii.hexlify(struct.pack('<b', version_id)))
+    # fout.write(binascii.hexlify(struct.pack('<i', patient_id)))
+    # fout.write(binascii.hexlify(struct.pack('<i', test_id)))
+    # # write the current time
+    # [fout.write(binascii.hexlify(struct.pack('<b', int(i)))) for i in line]
+    # #
+    # fout.write(binascii.hexlify(struct.pack('<i', frame_width)))
+    # fout.write(binascii.hexlify(struct.pack('<i', frame_height)))
+    # fout.write(binascii.hexlify(struct.pack('<i', number_of_frames)))
+    # fout.write(binascii.hexlify(struct.pack('<i', decay_point)))
+    # fout.write(binascii.hexlify(struct.pack('<i', heating_point)))
 
     # fout.close()
 
@@ -76,19 +83,19 @@ capture.RunGraph()
 while numpyArr is None:
     time.sleep(.1)
 
-
 buildHeader()
-printHeader()
+# printHeader()
 
 try:
-    for k in range(100):
-        fout.write("FRAME".encode('ascii'))
-        fout.write(binascii.hexlify(struct.pack('<i', k)))  # index of frame in 1 byte
+    for k in range(number_of_frames):
+        # fout.write("FRAME".encode('ascii'))
+        fout.write(struct.pack('<i', k))  # index of frame in 1 byte
         for x in range(0, numpyArr.shape[0]):
             for y in range(0, numpyArr.shape[1]):
-                numpyArr[x, y] = numpyArr[x,y] - 27315
+                numpyArr[x, y] = numpyArr[x, y] - 27315
                 print(numpyArr[x, y])
-                fout.write(binascii.hexlify(struct.pack('<h', numpyArr[x, y])))
+                fout.write(struct.pack('h', numpyArr[x, y]))
+                # fout.write(binascii.hexlify(struct.pack('<h', numpyArr[x, y])))
             tempDict[k] = numpyArr
     #     print("========================1")
     #     print(tempDict[k])
