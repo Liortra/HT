fc38d22965a9abb6465d5ab6d250d40f4db08154
liortra <lior.trachtman@s.afeka.ac.il>
2020-05-05 15:06:38 +0300

    Update project * add utils * change the camera window * fix bugs and code review

--- scripts/Main/HTBioApp.py
+++ scripts/Main/HTBioApp.py
@@ -1,3 +1,8 @@
+"""
+Assumptions:
+
+
+"""
 import cv2
 from kivy.app import App
 from kivy.clock import Clock
@@ -5,150 +10,143 @@ from kivy.graphics.texture import Texture
 from kivy.lang import Builder
 from kivy.uix.image import Image
 from kivy.uix.screenmanager import ScreenManager, Screen
-from datetime import datetime
-import os.path
+import sys
+
+sys.path.append("..")
+print(sys.path)
+from scripts.Main import Utils
 
 # init and const
 Builder.load_file('htbio.kv')
-# this section creates the date object
-dateTimeObj = datetime.now()
-now = dateTimeObj.strftime("%d%m%Y%H%M%S")  # ddMMyyyyHHmmss
-# path to scripts/HTBio_files
-basepath = os.path.dirname(__file__)
-nameFile = now + '.HTBio'
-nameVideo = now + '.avi'
-# Setting for saving the SMI video
-videoName = os.path.abspath(os.path.join(basepath, "..", "HTBio_files/", nameVideo))
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-out = cv2.VideoWriter(videoName, fourcc, 20.0, (640, 480))
-# Setting for saving HTBio file from Lepton cam
-filename = os.path.abspath(os.path.join(basepath, "..", "HTBio_files/", nameFile))
-file = open(filename, 'wb+')
-# cameraID2 = 0  # id of FLIR Lepton - check port 0 / 2
-cameraID1 = 1  # id of SMI Depstech - check port
+# cameraID1 = utils.find_camera()  # WARN 0 is from here - think what to do
+cameraID1 = 1
 
 
 class SMICamera(Image):
     def __init__(self, parent, capture, **kwargs):
         super(SMICamera, self).__init__(**kwargs)
         self.capture = capture  # data to read
-        self.parent = parent  # this object's parent (= box layout)
-        self.started = False  # start state
-        self.TurnOn = False  # to turn on camera
+        self.parent = parent  # this object's parent (= box layout) - change the logo to the camera
+        self.isRecording = False  # start recording video from SMI
+        self.opticVideoWriter = None  # init for video writer
 
     # starts the "Camera", capturing at 30 fps by default
-    def start(self, fps=30):
-        Clock.schedule_interval(self.update, 1.0 / fps)
+    def start(self, fps=30):  # TODO check fps
+        Clock.schedule_interval(self.update, 1.0 / fps)  # Schedule an event to be called every <timeout> seconds.
 
     # run a video on screen
-    def update(self, dt):
+    def update(self, dt):  # TypeError: schedule_interval() takes exactly 2 positional arguments
         ret, self.frame = self.capture.read()
         if ret:
-            if self.started:  # push on start button
-                out.write(self.frame)
-            # convert it to texture
-            image_texture = self.get_texture_from_frame(self.frame, 0)
-            # display image from the texture
-            self.texture = image_texture
-            self.parent.ids['imageCamera'].texture = self.texture
+            if self.isRecording:  # push on start button
+                self.opticVideoWriter.write(self.frame)  # TODO if writing with this cam don't work save it in list
+                # and then save
+            # convert it to texture & display image from the texture
+            self.parent.ids['imageCamera'].texture = self.get_texture_from_frame(self.frame, 0)
 
     def stop(self):
         Clock.unschedule(self.update)
 
     def get_texture_from_frame(self, frame, flipped):
-        buffer_frame = cv2.flip(frame, flipped)
-        buffer_frame_str = buffer_frame.tostring()
-        image_texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
-        image_texture.blit_buffer(buffer_frame_str, colorfmt='bgr', bufferfmt='ubyte')
-        return image_texture
+        bufferFrame = cv2.flip(frame, flipped)
+        bufferFrameStr = bufferFrame.tostring()
+        imageTexture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
+        imageTexture.blit_buffer(bufferFrameStr, colorfmt='bgr', bufferfmt='ubyte')
+        return imageTexture
 
-    def start_stop_RecordVideo(self):
-        self.started = not self.started
+    def start_stop_record_video(self):
+        self.isRecording = not self.isRecording
+
+    def init_record_writer(self, opticVideoWriter):
+        self.opticVideoWriter = opticVideoWriter
 
 
 class CameraScreen(Screen):
-    turnOn = False
-    heat = False
     # initialize the "Camera"
-    SMICamera = Image(source='logo.jpg')
+    isTurnOn = False
+    isHeated = False
+    isRecording = False
 
-    def startCamera(self, imageCamera, buttonTurnOn, buttonStart, buttonStop, buttonBack, buttonHeat):
+    # init and close the system
+    def init_camera(self, imageCamera, buttonTurnOn, buttonStart, buttonBack, buttonHeat):
         from scripts.API import LeptonAPI
-        from scripts.Arduino import on_off_led
-        if not self.turnOn:
+        from scripts.Arduino import LedController
+        if not self.isTurnOn:  # Setup for the system
             self.capture = cv2.VideoCapture(cameraID1)  # capture SMI cam
             self.SMICamera = SMICamera(self, self.capture)  # change the window from logo to camera
-            imageCamera = self.SMICamera
             self.SMICamera.start()
             # Set as started
-            self.turnOn = True
+            self.isTurnOn = True
             buttonTurnOn.text = 'Turn Off'
             # when started, let start enabled
-            buttonStop.disabled = False  # enabled
-            # Enable the Start (button)
-            buttonStart.disabled = False
-            # Prevent the back (button)
-            buttonBack.disabled = True
-            # initCam(filename, now) - delete this row
-            LeptonAPI.initCam()  # init Lepton cam - need to check if needed
-            buttonHeat.disabled = False # enabled
-            self.heat = True
-            on_off_led.initLed()  # start the Arduino
-        else:  # press on TurnOff
-            self.turnOn = False  # stop what was "started"
+            # buttonStop.disabled = False  # enabled
+            buttonStart.disabled = False  # Enable the Start (button)
+            buttonBack.disabled = True  # Prevent the back (button)
+            LeptonAPI.init_cam()  # init Lepton cam
+            buttonHeat.disabled = False  # enabled
+            LedController.init_led()  # start the Arduino
+        else:  # press on TurnOff = teardown for the system
+            self.isTurnOn = False  # stop what was "started"
             buttonTurnOn.text = 'Turn ON'
-            self.SMICamera.stop()
             # Reset camera to home image
             self.SMICamera.stop()
             self.SMICamera = Image(source='logo.jpg')
-            imageCamera.source = self.SMICamera.source
-            imageCamera.reload()
-            # Prevent the Stop (button)
-            buttonStop.disabled = True
-            # Prevent the Start (button)
-            buttonStart.disabled = True
-            # Enabled the back (button)
-            buttonBack.disabled = False
-            # Prevent the Heat (button)
+            imageCamera.source = self.SMICamera.source  # get back to the logo
+            imageCamera.reload()  # get back to the logo
+            # buttonStop.disabled = True  # Prevent the Stop (button)
+            buttonStart.disabled = True  # Prevent the Start (button)
+            buttonBack.disabled = False  # Enabled the back (button)
             buttonHeat.text = 'Heat'
-            buttonHeat.disabled = True
-            self.heat = False
-            # Release the capture
-            self.capture.release()
-
-    # start to make a video and run Lepton Camera
-    def startVideo(self):  # TODO need to check how disable stop button
-        if self.turnOn:  # Was running at click
-            self.SMICamera.start_stop_RecordVideo()  # start film a video
+            buttonHeat.disabled = True  # Prevent the Heat (button)
+            self.isHeated = False
+            self.capture.release()  # Release the capture
+            from scripts.Arduino import LedController
             from scripts.API import LeptonAPI
-            LeptonAPI.startLepton()  # start film a temp
-            # from scripts.Arduino import on_off_led
-            # on_off_led.startLed()  # start the Arduino
+            LeptonAPI.close_lepton()  # closing Lepton cam
+            LedController.close_led()  # closing Led
 
-    # stop the video
-    def stopVideo(self):  # TODO need to check how disable start button
-        self.SMICamera.start_stop_RecordVideo()
+    # Start recording or stop recording
+    def start_streaming(self, buttonTurnOn, buttonStart):  # TODO need to check how disable stop button & change name
+        if not self.isRecording:  # Was running at click
+            self.isRecording = True
+            buttonTurnOn.disabled = True # Disable the TurnOff (button)
+            buttonStart.text = 'Stop'
+            self.fileWriter, videoWriter, self.startTestTimeStamp = Utils.build_files()
+            self.SMICamera.init_record_writer(videoWriter)
+            self.SMICamera.start_stop_record_video()  # start film a video
+            from scripts.API import LeptonAPI
+            LeptonAPI.start_lepton()  # start film a temp
+            # from scripts.Arduino import ledController
+            # LedController.start_led()  # start the Arduino
+        else:
+            self.isRecording = False
+            buttonTurnOn.disabled = False  # Enable the TurnOff (button)
+            buttonStart.text = 'Start'
+            self.SMICamera.start_stop_record_video()
+            from scripts.API import LeptonAPI
+            LeptonAPI.stop_lepton(self.fileWriter, self.startTestTimeStamp)  # stop lepton film
+
+    def on_off_heat(self, buttonHeat): #
+        from scripts.Arduino import LedController
         from scripts.API import LeptonAPI
-        LeptonAPI.stopLepton(file, now)  # stop lepton film
-        # from scripts.Arduino import on_off_led
-        # on_off_led.stopLed()  # stop the Arduino
-
-    def ledHeat(self, buttonHeat):
-        if self.heat:
-            from scripts.Arduino import on_off_led
-            on_off_led.startLed()  # start the Arduino
+        if not self.isHeated:
+            LedController.start_led()  # start the Arduino
+            LeptonAPI.mark_heating_point()
+            self.isHeated = True
             buttonHeat.text = 'Cool'
-            self.heat = False
-        elif not self.heat:
-            from scripts.Arduino import on_off_led
-            on_off_led.stopLed()  # stop the Arduino
+        else:
+            LedController.stop_led()  # stop the Arduino
+            LeptonAPI.mark_decay_point()
+            self.isHeated = False
             buttonHeat.text = 'Heat'
-            self.heat = True
+
+    def close_camera(self):  # it's here for future function
+        print("Closing System")
 
 
 class MainScreen(Screen):
-    def exitApp(self):
-        MainApp.stop()
+    def exit_app(self):
+        App.get_running_app().stop()
 
 
 class MainApp(App):
@@ -161,9 +159,4 @@ class MainApp(App):
 
 # Start the MainApp
 if __name__ == '__main__':
-    import os, sys, inspect
-    currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-    parentdir = os.path.dirname(currentdir)
-    sys.path.insert(0, parentdir)
-    print(os.sys.path)
     MainApp().run()
